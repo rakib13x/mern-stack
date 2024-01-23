4979fb8c7d99b7a6601a46183d693d5392072cc9
rakib13x <rakib13x@gmail.com>
2024-01-24 00:14:51 +0600

    first commit...

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,17 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-estate.firebaseapp.com',
-  projectId: 'mern-estate',
-  storageBucket: 'mern-estate.appspot.com',
-  messagingSenderId: '1078482850952',
-  appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+  apiKey: "AIzaSyAqbcATXlqhUKCiLXt4nwmf2vIkbax1a9o",
+  authDomain: "house-renter-86cc2.firebaseapp.com",
+  projectId: "house-renter-86cc2",
+  storageBucket: "house-renter-86cc2.appspot.com",
+  messagingSenderId: "626020452918",
+  appId: "1:626020452918:web:be0577ba2c3b33ead3a697",
 };
-
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
